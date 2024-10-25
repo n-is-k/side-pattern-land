7e9546be80b5cebe49a3bcbba120ee4af2d0d855
n-is-k <57360746+n-is-k@users.noreply.github.com>
2024-10-25 18:55:08 +0200

    25 october

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,21 @@ import VideoPlayer from "./Components/VideoPlayer";
 import "./App.css";
 // import logo from "./images/side-spray-tr2.png";
 // import logo from "./images/side-spray-btr.png";
-import logo from "./images/echo-title.png";
+import logo from "./images/side-spray-tr2.png";
 
 const App = () => {
-
   const [isLoading, setIsLoading] = useState(false);
   const [formSubmitted, setFormSubmitted] = useState(false);
 
   const formRef = useRef(null);
 
-  const scriptUrl = 
-// "https://script.google.com/macros/s/AKfycbzXBEjRRJIREhfIgVxj5pUMkljpDYTMKq8rdEUPq9NYS99NUBiog6xjMnKRcGgiWTzY/exec"
-// "https://script.google.com/macros/s/AKfycbzxVlj5qv10deEw0RlYCi-aJPnEWR1GhIYKHzn1D4pJNH9DxCJOx3VO8u16GeNUkp9H/exec"
-// "https://script.google.com/macros/s/AKfycbyzIcVIDQksVy-S0o5gm5lUihfRUIx7yiwOZTqmYCDRaVWnpTNWHRPDW3_LzB6FxXjm/exec"
-// "https://script.google.com/macros/s/AKfycbymHAYbsm1QiZw9Jg38V7G5MH0d1cHrvK13K986T9zjFbFRDbOmTcP1pnv2vhoxN-3U/exec"
-// "https://script.google.com/macros/s/AKfycby9EqJTvGjLzDYoWMuNgLTKKaTTDYrfzylUGjANCDDCMf1iTRvgVtl6fbFEK5F7Lk_0/exec"npm
-"https://script.google.com/macros/s/AKfycbyI6H3FqH9F2yXSAwi1LQV2lHDoYdTzzF-jdMCQPvxi82YUYDPPeHMv2p8fBtzkqfVc/exec"
+  const scriptUrl =
+    // "https://script.google.com/macros/s/AKfycbzXBEjRRJIREhfIgVxj5pUMkljpDYTMKq8rdEUPq9NYS99NUBiog6xjMnKRcGgiWTzY/exec"
+    // "https://script.google.com/macros/s/AKfycbzxVlj5qv10deEw0RlYCi-aJPnEWR1GhIYKHzn1D4pJNH9DxCJOx3VO8u16GeNUkp9H/exec"
+    // "https://script.google.com/macros/s/AKfycbyzIcVIDQksVy-S0o5gm5lUihfRUIx7yiwOZTqmYCDRaVWnpTNWHRPDW3_LzB6FxXjm/exec"
+    // "https://script.google.com/macros/s/AKfycbymHAYbsm1QiZw9Jg38V7G5MH0d1cHrvK13K986T9zjFbFRDbOmTcP1pnv2vhoxN-3U/exec"
+    // "https://script.google.com/macros/s/AKfycby9EqJTvGjLzDYoWMuNgLTKKaTTDYrfzylUGjANCDDCMf1iTRvgVtl6fbFEK5F7Lk_0/exec"npm
+    "https://script.google.com/macros/s/AKfycbyI6H3FqH9F2yXSAwi1LQV2lHDoYdTzzF-jdMCQPvxi82YUYDPPeHMv2p8fBtzkqfVc/exec";
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -37,49 +36,54 @@ const App = () => {
   };
 
   return (
-
-
     <div className="app">
+      <div className="typing">
+        {" "}
+        <TypingText />
+      </div>
       {/* <VideoPlayer></VideoPlayer> */}
       <div>
-        <div className=""> 
-                  </div>
+        <div className=""></div>
         <div className="app--logo-wrapper">
           {/* <img src={logo} alt="logo" className="app--logo-image" /> */}
         </div>
-        {/* <div className="typing"> <TypingText /></div> */}
+        <div className="typing"> {/* <TypingText /> */}</div>
         <div className="sticker">
-          <img src={logo} alt="logo" className="app--logo-image"></img>
+          {/* <img src={logo} alt="logo" className="app--logo-image"></img> */}
         </div>
-        
       </div>
       <div className="form">
-          <div className="app--content">
+        <div className="app--content">
           {formSubmitted ? (
             <>
               {/* <p className="paragraph">Bless</p>  */}
-             <p className="paragraph">Submitted.</p> 
+              <p className="paragraph">Submitted.</p>
             </>
           ) : (
             <>
-              <form ref={formRef} onSubmit={handleSubmit} name="google-sheet" className="form">
-              <input
+              <form
+                ref={formRef}
+                onSubmit={handleSubmit}
+                name="google-sheet"
+                className="form"
+              >
+                <input
                   type="first"
                   name="first"
                   autocomplete="off"
                   placeholder="First Name"
                   onfocus="this.placeholder=''"
-                  onblur="this.placeholder = 'First Name'" 
+                  onblur="this.placeholder = 'First Name'"
                   required
                   className={`field ${isLoading ? "hidden" : ""}`}
                 />
-                  <input
+                <input
                   type="last"
                   name="last"
                   autocomplete="off"
                   placeholder="Last Name"
                   onfocus="this.placeholder=''"
-                  onblur="this.placeholder = 'Last Name'" 
+                  onblur="this.placeholder = 'Last Name'"
                   required
                   className={`field ${isLoading ? "hidden" : ""}`}
                 />
@@ -89,21 +93,24 @@ const App = () => {
                   autocomplete="off"
                   placeholder="Email"
                   onfocus="this.placeholder=''"
-                  onblur="this.placeholder = 'Email'" 
+                  onblur="this.placeholder = 'Email'"
                   required
                   className={`field ${isLoading ? "hidden" : ""}`}
                 />
-                <button type="submit" name="submit" className="button" disabled={isLoading}>
+                <button
+                  type="submit"
+                  name="submit"
+                  className="button"
+                  disabled={isLoading}
+                >
                   {isLoading ? "Please wait..." : "Submit"}
                 </button>
               </form>
             </>
           )}
-          
         </div>
-        </div>
+      </div>
     </div>
-    
   );
 };
 
--- src/Components/TypeWriter.jsx
+++ src/Components/TypeWriter.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import TypeWriter from 'react-typewriter';
+import React from "react";
+import TypeWriter from "react-typewriter";
 
 class TypingText extends React.Component {
-
   render() {
-
     return (
       // Passing in text.
-     
-      <TypeWriter typing={1} class={'typewriter'}>Side Pattern is a creative community developing special projects across art, design and fashion.<br/> Join to find out more.</TypeWriter> 
+
+      <TypeWriter typing={1} class={"typewriter"}>
+        Side Pattern is the art and design office of Kirtis Clarke currently
+        based in Amsterdam. It advances an approach to art and cultural
+        production that’s reflective of over 10 years of experience consulting
+        across art, design, culture, fashion, media and education.{" "}
+      </TypeWriter>
     );
   }
-  }
-
-
+}
 
 export default TypingText;
