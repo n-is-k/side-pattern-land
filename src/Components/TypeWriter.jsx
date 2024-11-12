55a4d8c5bd30dac1613872274b0af4aa9927aec7
n-is-k <57360746+n-is-k@users.noreply.github.com>
2024-11-12 23:27:50 +0100

    12 nov

--- src/Components/TypeWriter.jsx
+++ src/Components/TypeWriter.jsx
@@ -8,31 +8,34 @@ class TypingText extends React.Component {
 
       <div>
         {/* <TypeWriter typing={1.5} class={"typewriter"}>
-          Side Pattern is an art, design & media office based in Amsterdam. It
-          advances an approach to art and cultural production that is research
-          oriented and people focussed - reflective of over a decade of
-          experience initiating, consulting and leading on projects across art,
-          design, culture, fashion, media and education. Featured in GQ, Black
-          Discourse & Guap Magazine. Selected partners and clients include Nike,
-          New Balance, Daily Paper, Double A Magazine, Nieuwe Instituut, Dutch
-          Design Week, Sandberg Instituut, Design Academy Eindhoven, British
-          Council, Whitechapel Gallery, University of Bristol & International
-          Curators Forum.
+       Side Pattern is an art, design & media office based in Amsterdam. It
+          advances an approach to art and cultural production that reflects
+          what's happening outside and in our community. Reflective of over a
+          decade initiating, consulting and leading on projects across art,
+          design, culture, fashion, media and education the office provides a
+          bespoke consultancy service rooted in an ongoing line of cultural
+          research. Featured in Guap Magazine & The Black Discourse. Selected
+          partners and clients include Nike, New Balance, Daily Paper, Double A
+          Magazine, Nieuwe Instituut, Dutch Design Week, Sandberg Instituut,
+          Design Academy Eindhoven, British Council, Whitechapel Gallery,
+          University of Bristol & International Curators Forum. Side Pattern is
+          currently Kirtis Clarke, Zaina Pakabomba & Liam Wilson alongside a
+          network of artists, cultural practitioners, designers and researchers.
         </TypeWriter> */}
         <p>
           Side Pattern is an art, design & media office based in Amsterdam. It
           advances an approach to art and cultural production that reflects
-          what's happening outside. Reflective of over a decade initiating,
-          consulting and leading on projects across art, design, culture,
-          fashion, media and education the office provides a bespoke consultancy
-          service rooted in an ongoing line of cultural research.. Featured on
-          Guap Magazine & Black Discourse. Selected clients include Nike, New
-          Balance, Daily Paper, Double A Magazine, Nieuwe Instituut, Dutch
-          Design Week, Sandberg Instituut, Design Academy Eindhoven, British
-          Council, Whitechapel Gallery, University of Bristol & International
-          Curators Forum. Side Pattern is currently made up of Kirtis Clarke,
-          Zaina Pakabomba & Liam Wilson alongside our network of artists,
-          cultural practitioners designers and researchers.
+          what's happening outside and in our community. Reflective of over a
+          decade initiating, consulting and leading on projects across art,
+          design, culture, fashion, media and education the office provides a
+          bespoke consultancy service rooted in an ongoing line of cultural
+          research. Featured in Guap Magazine & The Black Discourse. Selected
+          partners and clients include Nike, New Balance, Daily Paper, Double A
+          Magazine, Nieuwe Instituut, Dutch Design Week, Sandberg Instituut,
+          Design Academy Eindhoven, British Council, Whitechapel Gallery,
+          University of Bristol & International Curators Forum. Side Pattern is
+          currently Kirtis Clarke, Zaina Pakabomba & Liam Wilson alongside a
+          network of artists, cultural practitioners, designers and researchers.
         </p>
       </div>
     );
